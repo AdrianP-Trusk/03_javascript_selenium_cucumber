69103515915d5b822d2a4a05a1aa17ef8112703d
adrianpothuaud <adrian@trusk.com>
2022-02-09 14:18:20 +0100

    refactored step defs

--- features/step_definitions/google/results_page_steps.js
+++ features/step_definitions/google/results_page_steps.js
@@ -4,6 +4,11 @@ const expect = require("expect")
 const webPages = require("../../../src/web-pages")
 const webUtils = require("../../../src/web-utils")
 
+Then("I am on Google Results page", async function() {
+    const actualBrowserUrl = await this.webdriver.getCurrentUrl()
+    expect(actualBrowserUrl).toEqual(expect.stringContaining("www.google.com/search?q=Hello+World"))
+})
+
 Then("The first result is Hello World Wikipedia article", async function() {
     const firstResultTitleElement = await webPages.google.results.getResultTitle(this.webdriver, "Hello world - Wikipédia")
     const firstResultLinkElement = await webUtils.getParentElement(this.webdriver, firstResultTitleElement)
--- features/step_definitions/google/search_page_steps.js
+++ features/step_definitions/google/search_page_steps.js
@@ -1,8 +1,12 @@
-const {Given} = require("@cucumber/cucumber")
+const {Given, When} = require("@cucumber/cucumber")
 
 const webPages = require("../../../src/web-pages")
 
 Given("I am on Google Search page", async function() {
     await this.webdriver.get("https://www.google.com")
     await webPages.google.search.closeCookiesPopup(this.webdriver)
+})
+
+When("I search for {string}", async function(searchText) {
+    await webPages.google.search.search(this.webdriver, "Hello World")
 })
